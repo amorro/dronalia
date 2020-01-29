3a310f4b214182184dc2cbe9367a5e454c3f396d
Francis Daniele <44406878+FrancisDaniele@users.noreply.github.com>
2020-01-29 10:34:00 +0100

    Add files via upload
    Añado id, nombre, y string para la url de la foto, a la clase skin, y creados: constructor con todos los campos menos tipo (lo elige el cliente), getters y setters, hash code and equals y ToString.  Francis

--- src/main/java/com/dronalia/dto/Skin.java
+++ src/main/java/com/dronalia/dto/Skin.java
@@ -3,10 +3,78 @@ package com.dronalia.dto;
 import com.dronalia.enumeradas.EnumSkinTematica;
 import com.dronalia.enumeradas.EnumSkinTipus;
 
+import java.util.Objects;
+
 public class Skin {
 
+    private int id;
+    private String name;
+    private String photo;
     private Enum<EnumSkinTipus> skinTipusEnum;
     private Enum<EnumSkinTematica> skinTematicaEnum;
 
+    public Skin(int id, String name, String photo, Enum<EnumSkinTematica> skinTematicaEnum) {
+        this.id = id;
+        this.name = name;
+        this.photo = photo;
+        this.skinTematicaEnum = skinTematicaEnum;
+    }
 
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPhoto() {
+        return photo;
+    }
+
+    public void setPhoto(String photo) {
+        this.photo = photo;
+    }
+
+    public Enum<EnumSkinTematica> getSkinTematicaEnum() {
+        return skinTematicaEnum;
+    }
+
+    public void setSkinTematicaEnum(Enum<EnumSkinTematica> skinTematicaEnum) {
+        this.skinTematicaEnum = skinTematicaEnum;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Skin skin = (Skin) o;
+        return id == skin.id &&
+                name.equals(skin.name) &&
+                photo.equals(skin.photo);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, photo);
+    }
+
+    @Override
+    public String toString() {
+        return "Skin{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", photo='" + photo + '\'' +
+                ", skinTipusEnum=" + skinTipusEnum +
+                ", skinTematicaEnum=" + skinTematicaEnum +
+                '}';
+    }
 }
