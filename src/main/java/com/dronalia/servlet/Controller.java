c36f5bb39d07933410796500e998bd2644ee2344
ams00 <ams00@iesemilidarder.com>
2020-01-29 17:47:29 +0100

    crear controller

---
+++ src/main/java/com/dronalia/servlet/Controller.java
@@ -0,0 +1,8 @@
+package com.dronalia.servlet;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+
+@WebServlet("/dronalia")
+public class Controller extends HttpServlet {
+}
